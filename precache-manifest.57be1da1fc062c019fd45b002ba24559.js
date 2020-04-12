953cb6c9c0c166ff88f45928034340ce665ec694
Haichuan Zhang <me@std4453.com>
2020-04-13 01:53:25 +0800

    Updates

---
+++ precache-manifest.57be1da1fc062c019fd45b002ba24559.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f13f5bf3efeb510e844279aaae705d7",
+    "url": "/betterdori/index.html"
+  },
+  {
+    "revision": "6117a6483cd2a9277fe1",
+    "url": "/betterdori/static/css/main.2ab15433.chunk.css"
+  },
+  {
+    "revision": "ed3a1b2b96546038165f",
+    "url": "/betterdori/static/js/2.fa8a2de0.chunk.js"
+  },
+  {
+    "revision": "df26508b26b9ba707c90907619d555f1",
+    "url": "/betterdori/static/js/2.fa8a2de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6117a6483cd2a9277fe1",
+    "url": "/betterdori/static/js/main.b9ac029d.chunk.js"
+  },
+  {
+    "revision": "d7b1ed55a3b314675ef5",
+    "url": "/betterdori/static/js/runtime-main.16a60e78.js"
+  },
+  {
+    "revision": "c352e7b670254267d30d47bc3a52b3b2",
+    "url": "/betterdori/static/media/D-DIN-Bold.c352e7b6.otf"
+  },
+  {
+    "revision": "d68d8669232a9e8215831889eba83db1",
+    "url": "/betterdori/static/media/D-DIN.d68d8669.otf"
+  },
+  {
+    "revision": "5f0631551a4b3aabc9e667f9d92ea683",
+    "url": "/betterdori/static/media/album.5f063155.svg"
+  },
+  {
+    "revision": "04bc19f80a941b5b3f3a55d748cdd0d0",
+    "url": "/betterdori/static/media/bars.04bc19f8.svg"
+  },
+  {
+    "revision": "f8a7f6392d07ad9ae46512ae0a61c705",
+    "url": "/betterdori/static/media/cancel.f8a7f639.svg"
+  },
+  {
+    "revision": "53b51969f847a6090455335bfff0936e",
+    "url": "/betterdori/static/media/cross.53b51969.svg"
+  },
+  {
+    "revision": "d440bb37d4ec5b75981cc38502425925",
+    "url": "/betterdori/static/media/d1.d440bb37.svg"
+  },
+  {
+    "revision": "70994b0565f9ec8483e8db4557ec3cb9",
+    "url": "/betterdori/static/media/d2.70994b05.svg"
+  },
+  {
+    "revision": "bed3323c6574c5cb8c13738ce69a1ef8",
+    "url": "/betterdori/static/media/d3.bed3323c.svg"
+  },
+  {
+    "revision": "9e36ff58081cadcd532180210db671db",
+    "url": "/betterdori/static/media/d4.9e36ff58.svg"
+  },
+  {
+    "revision": "0d8fa81a27b1ce04f06df1e1d919af5c",
+    "url": "/betterdori/static/media/d6.0d8fa81a.svg"
+  },
+  {
+    "revision": "c7296f0f8e8f45ff0d133e13418d95ab",
+    "url": "/betterdori/static/media/d8.c7296f0f.svg"
+  },
+  {
+    "revision": "5075b33e2b7d25b8c8aac283fbbbde2d",
+    "url": "/betterdori/static/media/delete.5075b33e.svg"
+  },
+  {
+    "revision": "2a28091b53b229d648ffda2a770653b3",
+    "url": "/betterdori/static/media/export.2a28091b.svg"
+  },
+  {
+    "revision": "190f0e5d07c2274e140626e627b8e430",
+    "url": "/betterdori/static/media/flick.190f0e5d.mp3"
+  },
+  {
+    "revision": "7b4a2e40f852e1f86a51ec55d2b87c05",
+    "url": "/betterdori/static/media/flick.7b4a2e40.svg"
+  },
+  {
+    "revision": "b3b0efea2cb6acf86714a17ae2e39c23",
+    "url": "/betterdori/static/media/focus.b3b0efea.svg"
+  },
+  {
+    "revision": "f06e8e6651ac356284fa41f83c0f72c6",
+    "url": "/betterdori/static/media/follow.f06e8e66.svg"
+  },
+  {
+    "revision": "833119f1663381714f15d7704f79721d",
+    "url": "/betterdori/static/media/hamburger.833119f1.svg"
+  },
+  {
+    "revision": "f76106f8ecd05fc57e655c3c9493da08",
+    "url": "/betterdori/static/media/import.f76106f8.svg"
+  },
+  {
+    "revision": "0fccf3e6c54afc7d3f9278f9860144e4",
+    "url": "/betterdori/static/media/marker.0fccf3e6.svg"
+  },
+  {
+    "revision": "9cf9cdb6474ed9ff0024ad77218db4d8",
+    "url": "/betterdori/static/media/pause.9cf9cdb6.svg"
+  },
+  {
+    "revision": "aa3ead5096a43e5b41ae71e8e31ade24",
+    "url": "/betterdori/static/media/play.aa3ead50.svg"
+  },
+  {
+    "revision": "07060722a91b129e9b00349c0559f508",
+    "url": "/betterdori/static/media/player.07060722.svg"
+  },
+  {
+    "revision": "c1d3a7c6a589cfc11505270fc9989496",
+    "url": "/betterdori/static/media/select.c1d3a7c6.svg"
+  },
+  {
+    "revision": "348c169b79fdc59c74e19bb27d07bfa8",
+    "url": "/betterdori/static/media/settings.348c169b.svg"
+  },
+  {
+    "revision": "710e706e3379cd6e501f52ccd94aeb91",
+    "url": "/betterdori/static/media/single.710e706e.svg"
+  },
+  {
+    "revision": "9c55945b93fd6ddc2130ceda557923ff",
+    "url": "/betterdori/static/media/slide-1.9c55945b.svg"
+  },
+  {
+    "revision": "e739354ff1db73566d866f63987f3e57",
+    "url": "/betterdori/static/media/slide-2.e739354f.svg"
+  },
+  {
+    "revision": "4360bbde42dfb479b0ae1948d91b796c",
+    "url": "/betterdori/static/media/submit.4360bbde.svg"
+  },
+  {
+    "revision": "343a1b1164e9f5fb28e1246588f9d624",
+    "url": "/betterdori/static/media/tap.343a1b11.mp3"
+  },
+  {
+    "revision": "593d700fe0e97e9b8228f7eeb2b84c5a",
+    "url": "/betterdori/static/media/timer.593d700f.svg"
+  },
+  {
+    "revision": "535eb49c8b7e1b160f9ba5669349aafc",
+    "url": "/betterdori/static/media/x05.535eb49c.svg"
+  },
+  {
+    "revision": "bedeac54840c9033bea2b8cd499bb135",
+    "url": "/betterdori/static/media/x10.bedeac54.svg"
+  },
+  {
+    "revision": "b4cf543abfcc29f65579d396a6190360",
+    "url": "/betterdori/static/media/x15.b4cf543a.svg"
+  },
+  {
+    "revision": "f8430c66d23d12f3432030d0257ab8b9",
+    "url": "/betterdori/static/media/x20.f8430c66.svg"
+  }
+]);
